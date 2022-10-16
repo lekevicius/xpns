fa4456f061837806bf2221bd61c67ff5820b2c65
smuueth <smuu.eth@proton.me>
2022-10-16 10:29:31 -0500

    use new deployment

--- owner-wallet/packages/react-app/src/contract/addresses.js
+++ owner-wallet/packages/react-app/src/contract/addresses.js
@@ -1,5 +1,5 @@
 // This address points to a dummy ERC-20 contract. Replace it with your own smart contracts.
 const addresses = {
-  wallet: "0x32276A549A1F9e10782Bc7f8Bf44623f7dbC625e",
+  wallet: "0x800697738b5a29b78ad26492e2505a233A6A8A6a",
 };
 export default addresses;
