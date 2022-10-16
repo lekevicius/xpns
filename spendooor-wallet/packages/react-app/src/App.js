0b618b79f025fa38b40fcd1997671f846b780b36
Jonas Lekevicius <jonas@lekevicius.com>
2022-10-15 19:42:41 -0500

    WalletConneeeeeect hooks

--- spendooor-wallet/packages/react-app/src/App.js
+++ spendooor-wallet/packages/react-app/src/App.js
@@ -1,13 +1,13 @@
-import { useQuery } from "@apollo/client";
 import { Contract } from "@ethersproject/contracts";
 import { shortenAddress, useCall, useEthers, useLookupAddress } from "@usedapp/core";
 import React, { useEffect, useState } from "react";
 
-import { Body, Button, Container, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+import { Body, Button, Container, Header } from "./components";
 
-import { addresses, abis } from "@my-app/contracts";
-import GET_TRANSFERS from "./graphql/subgraph";
+import useLocalStorage from 'react-use-localstorage';
+import { Wallet } from "ethers"
+
+// import { addresses, abis } from "@my-app/contracts";
 
 function WalletButton() {
   const [rendered, setRendered] = useState("");
@@ -48,25 +48,67 @@ function WalletButton() {
 }
 
 function App() {
+
+  /*
+  TODOS:
+
+  - Find simple useLocalStorage
+  - Check if wallet is already here
+  - If not, generate and save
+  - display public address
+  - allow entering public / private key to override
+  
+  - allow entering sc address
+  - save to localstorage
+  
+  - Setup ethers provider with wallet info
+  - Adjust config to point to ArbitrumGoerli
+
+  - paste WalletConnect and save connection
+  - get dispatch events
+  - send mint tx from tickets frontend
+  - receive tx and do something with it
+  */
+
+  const [xpnsPriv, setXpnsPriv] = useLocalStorage('xpns_priv');
+  const [xpnsContract, setXpnsContract] = useLocalStorage('xpns_contract');
+
+  let privateKey;
+  if (!xpnsPriv) {
+    const newWallet = Wallet.createRandom()
+    setXpnsPriv(newWallet.privateKey)
+    privateKey = newWallet.privateKey
+  } else {
+    privateKey = xpnsPriv
+  }
+  const wallet = new Wallet(privateKey);
+  const [addr, setAddr] = useState(wallet.address)
+
+  const [contractInput, setContractInput] = useState("");
+  const onSaveContractInput = () => {
+    setXpnsContract(contractInput)
+  }
+
+
   // Read more about useDapp on https://usedapp.io/
-  const { error: contractCallError, value: tokenBalance } =
-    useCall({
-       contract: new Contract(addresses.ceaErc20, abis.erc20),
-       method: "balanceOf",
-       args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
-    }) ?? {};
+  // const { error: contractCallError, value: tokenBalance } =
+  //   useCall({
+  //      contract: new Contract(addresses.ceaErc20, abis.erc20),
+  //      method: "balanceOf",
+  //      args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
+  //   }) ?? {};
 
-  const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
+  // const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
 
-  useEffect(() => {
-    if (subgraphQueryError) {
-      console.error("Error while querying subgraph:", subgraphQueryError.message);
-      return;
-    }
-    if (!loading && data && data.transfers) {
-      console.log({ transfers: data.transfers });
-    }
-  }, [loading, subgraphQueryError, data]);
+  // useEffect(() => {
+  //   if (subgraphQueryError) {
+  //     console.error("Error while querying subgraph:", subgraphQueryError.message);
+  //     return;
+  //   }
+  //   if (!loading && data && data.transfers) {
+  //     console.log({ transfers: data.transfers });
+  //   }
+  // }, [loading, subgraphQueryError, data]);
 
   return (
     <Container>
@@ -74,15 +116,16 @@ function App() {
         <WalletButton />
       </Header>
       <Body>
-        <Image src={logo} alt="ethereum-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <Link href="https://reactjs.org">
-          Learn React
-        </Link>
-        <Link href="https://usedapp.io/">Learn useDapp</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
+        <h3>{addr}</h3>
+        <div>
+          <input value={contractInput} onChange={e => setContractInput(e.target.value)} type="text" />
+          <button onClick={onSaveContractInput}>Save Contract Address</button>
+        </div>
+        <h3>{xpnsContract}</h3>
+        <div>
+          <input placeholder="Wallet Connect URL" type="text" />
+          <button>Connect with WalletConnect</button>
+        </div>
       </Body>
     </Container>
   );
